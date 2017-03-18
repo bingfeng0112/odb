a713d2bcd92f8833319acbb7e303acce7e6c19e4
hang199453031 <199453031@qq.com>
2017-03-18 11:38:38 +0800

    点击查看增加tac=new

--- odb/src/test/java/com/tianan/odb/grid_view/test_GridView_zhoubian_dirveruser_gas.java
+++ odb/src/test/java/com/tianan/odb/grid_view/test_GridView_zhoubian_dirveruser_gas.java
@@ -2,11 +2,13 @@ package com.tianan.odb.grid_view;
 import org.junit.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+
 import com.holmos.webtest.log.MyLogger;
 import com.holmos.webtest.utils.HolmosBaseUtils;
 import com.tianan.odb.android_pages.CreateActivityPages;
 import com.tianan.odb.configuration_device.ConfigurationAndroid;
 import com.tianan.odb.public_infunction.login_success;
+
 import io.appium.java_client.TouchAction;
 public class test_GridView_zhoubian_dirveruser_gas {
 	private static MyLogger logger = MyLogger.getLogger(test_GridView_zhoubian_dirveruser_gas.class);
@@ -23,15 +25,19 @@ public class test_GridView_zhoubian_dirveruser_gas {
 		TouchAction tac = new TouchAction(con.driver);
 		HolmosBaseUtils.sleep(2000);
 		//点击周边出行
+		 tac = new TouchAction(con.driver);
 		tac.tap(page.odb_GridView_zhoubian()).perform();
 		HolmosBaseUtils.sleep(2000);
 		//定位司机当前位置
+		 tac = new TouchAction(con.driver);
 		tac.tap(page.odb_GridView_zhoubian_driveruser()).perform();
 		HolmosBaseUtils.sleep(2000);
 		//点击加油站
+		 tac = new TouchAction(con.driver);
 		tac.tap(page.odb_GridView_zhoubian_gas()).perform();
 		HolmosBaseUtils.sleep(2000);
 		//点击查看
+		 tac = new TouchAction(con.driver);
 		tac.tap(page.odb_GridView_zhoubian_gas_check()).perform();
 		HolmosBaseUtils.sleep(2000);
 		//校验是否已进入加油站详情页面(执行脚本时，需要在硅谷亮城办公室)
