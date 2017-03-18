95b23c9f204950b8c0732ebd84c8f68b0adf60e4
hang199453031 <199453031@qq.com>
2017-03-18 14:09:02 +0800

    新增点击维修点

--- odb/src/test/java/com/tianan/odb/grid_view/test_GridView_zhoubian_driveruser_4s.java
+++ odb/src/test/java/com/tianan/odb/grid_view/test_GridView_zhoubian_driveruser_4s.java
@@ -1,14 +1,16 @@
 package com.tianan.odb.grid_view;
+import org.junit.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
+import com.holmos.webtest.log.MyLogger;
 import com.holmos.webtest.utils.HolmosBaseUtils;
 import com.tianan.odb.android_pages.CreateActivityPages;
 import com.tianan.odb.configuration_device.ConfigurationAndroid;
+import com.tianan.odb.public_infunction.TouchActionUtils;
 import com.tianan.odb.public_infunction.login_success;
-
-import io.appium.java_client.TouchAction;
 public class test_GridView_zhoubian_driveruser_4s {
-	
+    private static MyLogger logger = MyLogger.getLogger(test_GridView_zhoubian_driveruser_4s.class);
 	@Test
 	public void test_GridView_zhoubian_driveruser_4s(){
 		ConfigurationAndroid con = new ConfigurationAndroid();
@@ -17,23 +19,32 @@ public class test_GridView_zhoubian_driveruser_4s {
 		//调用ConfigurationAndroid预制方法
 		con.SetUp();
 		//进行登录
-		login.login(con.driver);
-		//使用appium下的TouchAction类中的，TouchAction方法，需要传driver
-		TouchAction tac = new TouchAction(con.driver);
+		login.login(ConfigurationAndroid.driver);
+		//调取点击方法
+		TouchActionUtils tau = new TouchActionUtils();
 		HolmosBaseUtils.sleep(2000);
 		//点击周边出行
-		 tac = new TouchAction(con.driver);
-		tac.tap(page.odb_GridView_zhoubian()).perform();
+		tau.tap(page.odb_GridView_zhoubian());
 		HolmosBaseUtils.sleep(2000);
 		//定位司机当前位置
-		 tac = new TouchAction(con.driver);
-		tac.tap(page.odb_GridView_zhoubian_driveruser()).perform();
+		tau.tap(page.odb_GridView_zhoubian_driveruser());
 		HolmosBaseUtils.sleep(2000);
 		//点击维修点
-		
-		
-		
-		
+		tau.tap(page.odb_GridView_zhoubian_4s());
+		HolmosBaseUtils.sleep(2000);
+		//点击查看按钮
+		tau.tap(page.odb_GridView_zhoubian_4s_check());
+		HolmosBaseUtils.sleep(2000);
+		//校验是否已进入维修点详情页面(执行脚本时，需要在硅谷亮城办公室)
+		Assert.assertEquals(page.odb_GridView_zhoubian_4s_check_name().getText(), "北京上地四方汽车销售服务有限公司");
+		HolmosBaseUtils.sleep(1000);
+			
+	}
+	@AfterTest
+	public void tearDown() throws Exception{
+	
+	ConfigurationAndroid.driver.quit();
+	logger.info("资源释放成功");
 	}
 
 }
